91e1b2429a856125e14cc6991368dda351e097fb
ZSathiya99 <sathiyavijaya1999@gmail.com>
2025-11-04 15:35:00 +0530

    New Changes commit

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173", "https://your-frontend-domain.com"], // allow both local + deployed frontend
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true, // if using cookies or auth headers
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
